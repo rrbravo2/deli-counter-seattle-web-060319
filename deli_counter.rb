ebb3f557b1372f49fe7c6f2bec3a05e8f7b454d6
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 05:21:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,11 @@ def line(array)
  if array.length == 0
    puts "The line is currently empty."
  else
-   current_line = " "
+   current_line = ""
    array.each_with_index do |name, index|
      current_line << "#{index + 1}. #{name}"
  end
-  puts "The line is currently:#{current_line}."
+  puts "The line is currently:#{current_line}"
  end
 end
 
