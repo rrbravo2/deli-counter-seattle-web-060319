30720b52a9e85e82bb46141bd128e76b82d05a9a
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 05:17:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,8 @@ def line(array)
   puts "The line is currently:#{current_line}"
  end
 end 
+
+def take_a_number(array, name)
+ array << name
+ puts "Welcome, #{name}. You are number #{array.length} in line."
+end
