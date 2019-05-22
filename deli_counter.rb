43ca64e06eb8480db34f61436e8f5a7a5f93e1b1
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 05:24:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,12 @@ def take_a_number(array, name)
  array << name
  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
+
+def now_serving(array)
+ if array.length > 0
+   puts "Currently serving #{array[0]}."
+   array.shift
+ else
+   puts "There is nobody waiting to be served!"
+ end
+end 
