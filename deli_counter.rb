00fc789cfd8c00d02815c32eb01d80496bcfa0cd
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 05:14:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(array)
+ if array.length == 0
+   puts "The line is currently empty."
+ else
+   current_line = ""
+   array.each_with_index do |name, index|
+     current_line << "#{index + 1}. #{name}"
+ end# Write your code here.
+  puts "The line is currently:#{current_line}"
+ end
+end 
